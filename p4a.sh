fe0c441264cd8f622422595feb11859d9268efe3
Balaji Venkataraman <xbala.jiv@gmail.com>
2014-12-19 11:15:31 -0800

    Update and rename p4a-steps to p4a.sh

---
+++ p4a.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/xbalaji/pub/master/p4a-scripts/ubuntu-clean.sh
+wget https://raw.githubusercontent.com/xbalaji/pub/master/p4a-scripts/ub1.sh
+wget https://raw.githubusercontent.com/xbalaji/pub/master/p4a-scripts/ub2.sh
+wget https://raw.githubusercontent.com/xbalaji/pub/master/p4a-scripts/ub3.sh
+wget https://raw.githubusercontent.com/xbalaji/pub/master/p4a-scripts/ub4.sh
+wget https://raw.githubusercontent.com/xbalaji/pub/master/p4a-scripts/ub5.sh
