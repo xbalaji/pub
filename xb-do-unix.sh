4160811322bbd78b90effbfd4b46bc56027d3f5c
Balaji Venkataraman <xbala.jiv@gmail.com>
2015-07-03 09:06:06 -0700

    fixed execute permission for static ip script and added backslash

--- xb-do-unix.sh
+++ xb-do-unix.sh
@@ -70,12 +70,13 @@ m_line=\$(echo \$ip_line | awk '{print \$4}' | sed -e 's/Mask:/   netmask /')
 b_line=\$(echo \$ip_line | awk '{print \$3}' | sed -e 's/Bcast:/   broadcast /')
 g_line=\$(route -n | grep '^0' | awk '{print \$2}' | sed -e 's/^/   gateway /')
 
-dns_servers=$(grep nameserver /etc/resolv.conf | awk '{printf "%s ", \$2}')
+dns_servers=\$(grep nameserver /etc/resolv.conf | awk '{printf "%s ", \$2}')
 d_line=\$(echo "   dns-nameservers \$dns_servers")
 static_line="\n\$iface_s\n\$a_line\n\$m_line\n\$b_line\n\$g_line\n\$d_line\n"
 
 sed -i -e "s,\$iface_d,\${static_line},g" /etc/network/interfaces
 EOF
+chmod +x $CFG_ST_IP_SCRIPT
 
 # append to bashrc
 cat << EOF >> $BASHRC_FILE
@@ -170,8 +171,8 @@ set ai
 set directory=.,/tmp,/var/tmp,.,~/tmp
 set paste
 
-map  :n
-map  :N
+map  :n
+map  :N
 
 noremap 5 %
 noremap % 5
